8478c02060d1e327b351284bece598778ce011f2
knknkn <knknkn1162@gmail.com>
2018-11-27 15:29:04 +0900

    impl dcache

---
+++ elem/dcache.vhdl
@@ -0,0 +1,67 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use STD.TEXTIO.ALL;
+use work.tools_pkg.ALL;
+
+entity dcache is
+  generic(filename : string);
+  port (
+    clk, rst, load : in std_logic;
+    we : in std_logic;
+    -- program counter is 4-byte aligned
+    a : in std_logic_vector(29 downto 0);
+    wd : in std_logic_vector(31 downto 0);
+    rd : out std_logic_vector(31 downto 0)
+  );
+end entity;
+
+architecture behavior of dcache is
+  -- The size of data cache assumes to be 1K-byte
+  constant SIZE : natural := 256; -- 0x0100
+  type ramtype is array(natural range<>) of std_logic_vector(31 downto 0);
+  signal ram : ramtype(0 to SIZE-1);
+
+begin
+  process(clk, rst, a)
+    file memfile : text open READ_MODE is filename;
+    variable idx : integer;
+    variable lin : line;
+    variable ch : character;
+   begin
+    -- initialization
+    if rst = '1' then
+      -- initialize with zeros
+      ram <= (others => (others => '0'));
+    elsif rising_edge(clk) then
+      if load = '1' then
+        idx := 0;
+        while not endfile(memfile) loop
+          readline(memfile, lin);
+          for i in 0 to 7 loop
+            read(lin, ch);
+            ram(idx)(31-i*4 downto 28-i*4) <= char2bits(ch);
+          end loop;
+          idx := idx + 1;
+        end loop;
+        file_close(memfile);
+      elsif we = '1' then
+        if not is_X(a) then
+          ram(to_integer(unsigned(a))) <= wd;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(clk, rst, a, we)
+  begin
+    -- read
+    if is_X(a) then
+      rd <= (others => '0');
+    else
+      if we = '0' then
+        rd <= ram(to_integer(unsigned(a)));
+      end if;
+    end if;
+  end process;
+end architecture;
---
+++ elem/dcache_tb.vhdl
@@ -0,0 +1,74 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.tools_pkg.ALL;
+
+entity dcache_tb is
+end entity;
+
+architecture testbench of dcache_tb is
+  component dcache
+    generic(filename : string);
+    port (
+      clk, rst, load : in std_logic;
+      we : in std_logic;
+      -- program counter is 4-byte aligned
+      a : in std_logic_vector(29 downto 0);
+      wd : in std_logic_vector(31 downto 0);
+      rd : out std_logic_vector(31 downto 0)
+    );
+  end component;
+
+  signal clk, rst, load, we : std_logic;
+  signal a : std_logic_vector(29 downto 0);
+  signal wd, rd : std_logic_vector(31 downto 0);
+  constant clk_period : time := 10 ns;
+  signal stop : boolean;
+  constant filename : string := "./assets/mem/memfile.hex";
+
+begin
+  uut : dcache generic map (filename=>filename)
+  port map (
+    clk => clk, rst => rst, load => load,
+    we => we,
+    a => a,
+    wd => wd,
+    rd => rd
+  );
+
+  clk_process: process
+  begin
+    while not stop loop
+      clk <= '0'; wait for clk_period/2;
+      clk <= '1'; wait for clk_period/2;
+    end loop;
+    wait;
+  end process;
+
+  stim_proc : process
+  begin
+    wait for clk_period;
+    rst <= '1'; wait for 1 ns; rst <= '0';
+
+    -- sync
+    load <= '1'; wait for 1 ns;
+    wait for clk_period/2;
+    load <= '0';
+    we <= '0';
+    wait until falling_edge(clk);
+    -- read test
+    a <= b"00" & X"0000000"; wait for 1 ns; assert rd /= X"00000000";
+    a <= b"00" & X"0000001"; wait for 1 ns; assert rd /= X"00000000";
+
+    wait until falling_edge(clk);
+    -- write in ram
+    we <= '1'; a <= b"00" & X"0000002"; wd <= X"FFFFFFFF"; wait for clk_period/2+ 1 ns;
+    we <= '0'; wait for 1 ns; assert rd = X"FFFFFFFF";
+    -- skip
+    stop <= TRUE;
+    -- success message
+    assert false report "end of test" severity note;
+    wait;
+  end process;
+
+end architecture;
