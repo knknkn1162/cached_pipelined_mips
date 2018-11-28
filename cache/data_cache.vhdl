b41b3e7b6733232b67214d80b024e1cdc4cf3eff
knknkn <knknkn1162@gmail.com>
2018-11-29 00:55:00 +0900

    impl mem & data_cache

--- cache/data_cache.vhdl
+++ cache/data_cache.vhdl
@@ -13,10 +13,11 @@ entity data_cache is
     a : in std_logic_vector(31 downto 0);
     wd : in std_logic_vector(31 downto 0);
     rd : out std_logic_vector(31 downto 0);
-    load_d1, load_d2, load_d3, load_d4, load_d5, load_d6, load_d7, load_d8 : in std_logic_vector(31 downto 0);
-    load_ok : in std_logic;
-    dump_d1, dump_d2, dump_d3, dump_d4, dump_d5, dump_d6, dump_d7, dump_d8 : out std_logic_vector(31 downto 0);
-    dump_ok: out std_logic
+    im_d1, im_d2, im_d3, im_d4, im_d5, im_d6, im_d7, im_d8 : in std_logic_vector(31 downto 0);
+    ex_d1, ex_d2, ex_d3, ex_d4, ex_d5, ex_d6, ex_d7, ex_d8 : out std_logic_vector(31 downto 0);
+    ex_tag : out std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
+    ex_index : out std_logic_vector(CONST_CACHE_INDEX_SIZE-1 downto 0);
+    ex_ok: out std_logic
   );
 end entity;
 
@@ -46,10 +47,27 @@ architecture behavior of data_cache is
         );
   end component;
 
+  component flopr_en
+    generic(N : natural);
+    port (
+      clk, rst, en: in std_logic;
+      a : in std_logic_vector(N-1 downto 0);
+      y : out std_logic_vector(N-1 downto 0)
+    );
+  end component;
+
+  -- state
+  type statetype is (
+    NormalS, DumpS, WaitS, LoadS
+  );
+  signal state, nextstate : statetype;
+
   -- The size of data cache assumes to be 1K-byte
   constant SIZE : natural := 256; -- 0x0100
+  constant DATA_BLOCK_SIZE : natural := 2**CONST_CACHE_OFFSET_SIZE;
   type validtype is array(natural range<>) of std_logic;
   type ramtype is array(natural range<>) of std_logic_vector(31 downto 0);
+  type addr30_type is array(natural range<>) of std_logic_vector(29 downto 0);
   type tagtype is array(natural range<>) of std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
 
   signal valid_data : validtype(0 to SIZE-1);
@@ -66,15 +84,11 @@ architecture behavior of data_cache is
   signal ram8_data : ramtype(0 to SIZE-1);
 
   -- decode addr
-  signal tag : std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
-  signal index : std_logic_vector(CONST_CACHE_INDEX_SIZE-1 downto 0);
-  signal offset : std_logic_vector(CONST_CACHE_OFFSET_SIZE-1 downto 0);
+  signal addr_tag : std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
+  signal addr_index : std_logic_vector(CONST_CACHE_INDEX_SIZE-1 downto 0);
+  signal addr_offset : std_logic_vector(CONST_CACHE_OFFSET_SIZE-1 downto 0);
 
-  -- load from memory in case of cache miss
-  signal dump : std_logic;
-  signal s : std_logic_vector(2 downto 0); -- selector for mux8
-
-  signal ram0_datum : std_logic_vector(31 downto 0);
+  -- TODO: compatible with CONST_CACHE_OFFSET_SIZE
   signal ram1_datum : std_logic_vector(31 downto 0);
   signal ram2_datum : std_logic_vector(31 downto 0);
   signal ram3_datum : std_logic_vector(31 downto 0);
@@ -83,17 +97,50 @@ architecture behavior of data_cache is
   signal ram6_datum : std_logic_vector(31 downto 0);
   signal ram7_datum : std_logic_vector(31 downto 0);
   signal ram8_datum : std_logic_vector(31 downto 0);
+  signal tag_datum : std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
+
+  -- is cache miss occurs or not
+  signal cache_miss_en : std_logic;
+  signal rd_s : std_logic_vector(2 downto 0); -- selector for mux8
 
 begin
+  process(clk, rst, nextstate)
+  begin
+    if rst = '1' then
+      -- initialization
+      state <= NormalS;
+    elsif rising_edge(clk) then
+      state <= nextstate;
+    end if;
+  end process;
+
+  process(state)
+  begin
+    case state is
+      when NormalS =>
+        if cache_miss_en = '1' then
+          nextstate <= DumpS;
+        else
+          nextstate <= NormalS;
+        end if;
+      when DumpS =>
+        nextstate <= LoadS;
+      when LoadS =>
+        nextstate <= NormalS;
+      when others =>
+        nextstate <= NormalS;
+    end case;
+  end process;
+
   cache_decoder0 : cache_decoder port map(
     addr => a,
-    tag => tag,
-    index => index,
-    offset => offset
+    tag => addr_tag,
+    index => addr_index,
+    offset => addr_offset
   );
 
   -- write data or load block from memory
-  process(clk, rst, load_ok, index, load_d1, load_d2, load_d3, load_d4, load_d5, load_d6, load_d7, load_d8)
+  process(clk, rst, we, addr_index, im_d1, im_d2, im_d3, im_d4, im_d5, im_d6, im_d7, im_d8)
     variable idx : natural;
   begin
     -- initialization
@@ -102,49 +149,48 @@ begin
       valid_data <= (others => '0');
     elsif rising_edge(clk) then
       -- pull the notification from the memory
-      if load_ok = '1' then
-        idx := to_integer(unsigned(index));
-        ram1_data(idx) <= load_d1;
-        ram2_data(idx) <= load_d2;
-        ram3_data(idx) <= load_d3;
-        ram4_data(idx) <= load_d4;
-        ram5_data(idx) <= load_d5;
-        ram6_data(idx) <= load_d6;
-        ram7_data(idx) <= load_d7;
-        ram8_data(idx) <= load_d8;
+      if we = '1' then
+        idx := to_integer(unsigned(addr_index));
+        ram1_data(idx) <= im_d1;
+        ram2_data(idx) <= im_d2;
+        ram3_data(idx) <= im_d3;
+        ram4_data(idx) <= im_d4;
+        ram5_data(idx) <= im_d5;
+        ram6_data(idx) <= im_d6;
+        ram7_data(idx) <= im_d7;
+        ram8_data(idx) <= im_d8;
       end if;
     end if;
   end process;
 
-  process(index, offset, we)
-    variable s0 : std_logic_vector(2 downto 0);
-    variable dump0 : std_logic;
+  process(addr_index, addr_offset, we)
+    variable cache_miss_en0 : std_logic;
   begin
-    dump0 := '0';
+    cache_miss_en0 := '0';
     if we = '0' then
-      if valid_data(to_integer(unsigned(index))) = '1' then
+      if valid_data(to_integer(unsigned(addr_index))) = '1' then
         -- cache hit!
-        if tag_data(to_integer(unsigned(index))) = tag then
-          s0 := offset;
+        if tag_data(to_integer(unsigned(addr_index))) = addr_tag then
+          rd_s <= addr_offset;
         else
           -- cache miss
-          dump0 := '1';
+          cache_miss_en0 := '1';
         end if;
       end if;
     end if;
-    s <= s0;
-    dump <= dump0;
+    cache_miss_en <= cache_miss_en0;
   end process;
 
   -- output rd signal
-  ram1_datum <= ram1_data(to_integer(unsigned(index)));
-  ram2_datum <= ram2_data(to_integer(unsigned(index)));
-  ram3_datum <= ram3_data(to_integer(unsigned(index)));
-  ram4_datum <= ram4_data(to_integer(unsigned(index)));
-  ram5_datum <= ram5_data(to_integer(unsigned(index)));
-  ram6_datum <= ram6_data(to_integer(unsigned(index)));
-  ram7_datum <= ram7_data(to_integer(unsigned(index)));
-  ram8_datum <= ram8_data(to_integer(unsigned(index)));
+  ram1_datum <= ram1_data(to_integer(unsigned(addr_index)));
+  ram2_datum <= ram2_data(to_integer(unsigned(addr_index)));
+  ram3_datum <= ram3_data(to_integer(unsigned(addr_index)));
+  ram4_datum <= ram4_data(to_integer(unsigned(addr_index)));
+  ram5_datum <= ram5_data(to_integer(unsigned(addr_index)));
+  ram6_datum <= ram6_data(to_integer(unsigned(addr_index)));
+  ram7_datum <= ram7_data(to_integer(unsigned(addr_index)));
+  ram8_datum <= ram8_data(to_integer(unsigned(addr_index)));
+
   mux8_0 : mux8 generic map(N=>32)
   port map (
     d000 => ram1_datum,
@@ -155,25 +201,73 @@ begin
     d101 => ram6_datum,
     d110 => ram7_datum,
     d111 => ram8_datum,
-    s => s,
+    s => rd_s,
     y => rd
   );
 
-  process(index, dump, valid_data)
-    variable idx : natural;
-  begin
-    if (valid_data(to_integer(unsigned(index))) = '1' and dump = '1') then
-      idx := to_integer(unsigned(index));
-      dump_d1 <= ram1_data(idx);
-      dump_d2 <= ram2_data(idx);
-      dump_d3 <= ram3_data(idx);
-      dump_d4 <= ram4_data(idx);
-      dump_d5 <= ram5_data(idx);
-      dump_d6 <= ram6_data(idx);
-      dump_d7 <= ram7_data(idx);
-      dump_d8 <= ram8_data(idx);
-      -- notify the dump completion to the memory
-      dump_ok <= '1';
-    end if;
-  end process;
+  tag_datum <= tag_data(to_integer(unsigned(addr_index)));
+  -- save
+  reg_ex_tag : flopr_en generic map (N=>CONST_CACHE_INDEX_SIZE)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => tag_datum,
+    y => ex_tag
+  );
+  ex_index <= addr_index;
+
+  reg_ex_d1 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram1_datum,
+    y => ex_d1
+  );
+
+  reg_ex_d2 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram2_datum,
+    y => ex_d2
+  );
+
+  reg_ex_d3 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram3_datum,
+    y => ex_d3
+  );
+
+  reg_ex_d4 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram4_datum,
+    y => ex_d4
+  );
+
+  reg_ex_d5 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram5_datum,
+    y => ex_d5
+  );
+
+  reg_ex_d6 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram6_datum,
+    y => ex_d6
+  );
+
+  reg_ex_d7 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram7_datum,
+    y => ex_d8
+  );
+
+  reg_ex_d8 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => cache_miss_en,
+    a => ram8_datum,
+    y => ex_d8
+  );
 end architecture;
--- elem/mem.vhdl
+++ elem/mem.vhdl
@@ -9,34 +9,56 @@ entity mem is
   generic(filename : string; BITS : natural);
   port (
     clk, rst, load : in std_logic;
+    -- we='1' when transport cache2mem
     we : in std_logic;
-    -- program counter is 4-byte aligned
-    a : in std_logic_vector(CONST_CACHE_TAG_SIZE+CONST_CACHE_INDEX_SIZE-1 downto 0);
+    tag : in std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
+    index : in std_logic_vector(CONST_CACHE_INDEX_SIZE-1 downto 0);
     wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8 : in std_logic_vector(31 downto 0);
-    rd1, rd2, rd3, rd4, rd5, rd6, rd7, rd8 : out std_logic_vector(31 downto 0)
+    rd1, rd2, rd3, rd4, rd5, rd6, rd7, rd8 : out std_logic_vector(31 downto 0);
+    rok : out std_logic;
+    rd_en : out std_logic
   );
 end entity;
 
 architecture behavior of mem is
+
+  component flopr_en
+    generic(N : natural);
+    port (
+      clk, rst, en: in std_logic;
+      a : in std_logic_vector(N-1 downto 0);
+      y : out std_logic_vector(N-1 downto 0)
+    );
+  end component;
   constant SIZE : natural := 2**BITS;
   type ram_type is array(natural range<>) of std_logic_vector(31 downto 0);
   type addr30_type is array(natural range<>) of std_logic_vector(29 downto 0);
   signal ram : ram_type(0 to SIZE-1);
   signal stored_addr : addr30_type(0 to 2**CONST_CACHE_OFFSET_SIZE-1);
-  signal test_vec : std_logic_vector(31 downto 0);
+  signal rd_en0 : std_logic;
+
+  -- TODO: compatible with CONST_CACHE_OFFSET_SIZE
+  signal ram1_datum : std_logic_vector(31 downto 0);
+  signal ram2_datum : std_logic_vector(31 downto 0);
+  signal ram3_datum : std_logic_vector(31 downto 0);
+  signal ram4_datum : std_logic_vector(31 downto 0);
+  signal ram5_datum : std_logic_vector(31 downto 0);
+  signal ram6_datum : std_logic_vector(31 downto 0);
+  signal ram7_datum : std_logic_vector(31 downto 0);
+  signal ram8_datum : std_logic_vector(31 downto 0);
 
 begin
   m : for i in 0 to 2**CONST_CACHE_OFFSET_SIZE-1 generate
-    stored_addr(i) <= a & std_logic_vector(to_unsigned(i, 3));
+    stored_addr(i) <= tag & index & std_logic_vector(to_unsigned(i, 3));
   end generate;
 
-  process(clk, rst, a)
+  process(clk, rst, load, stored_addr, wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8)
     file memfile : text open READ_MODE is filename;
     variable idx : integer;
     variable lin : line;
     variable ch : character;
     variable res : std_logic_vector(3 downto 0);
-   begin
+  begin
     -- initialization
     if rst = '1' then
       -- initialize with zeros
@@ -54,7 +76,7 @@ begin
         end loop;
         file_close(memfile);
       elsif we = '1' then
-        if not is_X(a) then
+        if not is_X(stored_addr(0)) then
           ram(to_integer(unsigned(stored_addr(0)))) <= wd1;
           ram(to_integer(unsigned(stored_addr(1)))) <= wd2;
           ram(to_integer(unsigned(stored_addr(2)))) <= wd3;
@@ -69,26 +91,86 @@ begin
   end process;
 
   -- read block
-  process(clk, rst, stored_addr, we)
+  process(stored_addr, we)
   begin
     if is_X(stored_addr(0)) then
-      rd1 <= (others => '0');
-      rd2 <= (others => '0');
-      rd3 <= (others => '0');
-      rd4 <= (others => '0');
-      rd5 <= (others => '0');
-      rd6 <= (others => '0');
-      rd7 <= (others => '0');
-      rd8 <= (others => '0');
+      ram1_datum <= (others => '0');
+      ram2_datum <= (others => '0');
+      ram3_datum <= (others => '0');
+      ram4_datum <= (others => '0');
+      ram5_datum <= (others => '0');
+      ram6_datum <= (others => '0');
+      ram7_datum <= (others => '0');
+      ram8_datum <= (others => '0');
     elsif we = '0' then
-      rd1 <= ram(to_integer(unsigned(stored_addr(0))));
-      rd2 <= ram(to_integer(unsigned(stored_addr(1))));
-      rd3 <= ram(to_integer(unsigned(stored_addr(2))));
-      rd4 <= ram(to_integer(unsigned(stored_addr(3))));
-      rd5 <= ram(to_integer(unsigned(stored_addr(4))));
-      rd6 <= ram(to_integer(unsigned(stored_addr(5))));
-      rd7 <= ram(to_integer(unsigned(stored_addr(6))));
-      rd8 <= ram(to_integer(unsigned(stored_addr(7))));
+      ram1_datum <= ram(to_integer(unsigned(stored_addr(0))));
+      ram2_datum <= ram(to_integer(unsigned(stored_addr(1))));
+      ram3_datum <= ram(to_integer(unsigned(stored_addr(2))));
+      ram4_datum <= ram(to_integer(unsigned(stored_addr(3))));
+      ram5_datum <= ram(to_integer(unsigned(stored_addr(4))));
+      ram6_datum <= ram(to_integer(unsigned(stored_addr(5))));
+      ram7_datum <= ram(to_integer(unsigned(stored_addr(6))));
+      ram8_datum <= ram(to_integer(unsigned(stored_addr(7))));
+      -- notify completion of export to the cache
+      rd_en0 <= '1';
     end if;
   end process;
+  rd_en <= rd_en0;
+
+  -- transport rds to cache
+  reg_d1 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram1_datum,
+    y => rd1
+  );
+
+  reg_d2 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram2_datum,
+    y => rd2
+  );
+
+  reg_d3 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram3_datum,
+    y => rd3
+  );
+
+  reg_d4 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram4_datum,
+    y => rd4
+  );
+
+  reg_d5 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram5_datum,
+    y => rd5
+  );
+
+  reg_d6 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram6_datum,
+    y => rd6
+  );
+
+  reg_d7 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram7_datum,
+    y => rd7
+  );
+
+  reg_d8 : flopr_en generic map (N=>32)
+  port map (
+    clk => clk, rst => rst, en => rd_en0,
+    a => ram8_datum,
+    y => rd8
+  );
 end architecture;
