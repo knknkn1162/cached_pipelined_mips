28cd8f38be7a67786ca712427233fc2f5a5147c3
knknkn <knknkn1162@gmail.com>
2018-12-01 23:06:24 +0900

    fix data_cache testbench for adding valid_flag signal

--- cache/data_cache_tb.vhdl
+++ cache/data_cache_tb.vhdl
@@ -23,6 +23,7 @@ architecture testbench of data_cache_tb is
       rd_index : out std_logic_vector(CONST_CACHE_INDEX_SIZE-1 downto 0);
       -- push cache miss to the memory
       cache_miss_en : out std_logic;
+      valid_flag : out std_logic;
       -- pull load from the memory
       load_en : in std_logic
     );
@@ -37,7 +38,7 @@ architecture testbench of data_cache_tb is
   signal rd_tag : std_logic_vector(CONST_CACHE_TAG_SIZE-1 downto 0);
   signal rd_index : std_logic_vector(CONST_CACHE_INDEX_SIZE-1 downto 0);
 
-  signal tag_s, cache_miss_en, load_en : std_logic;
+  signal tag_s, cache_miss_en, load_en, valid_flag : std_logic;
   constant clk_period : time := 10 ns;
   signal stop : boolean;
   constant filename : string := "./assets/mem/memfile.hex";
@@ -57,7 +58,7 @@ begin
     tag_s => tag_s,
     rd_tag => rd_tag,
     rd_index => rd_index,
-    cache_miss_en => cache_miss_en,
+    cache_miss_en => cache_miss_en, valid_flag => valid_flag,
     load_en => load_en
   );
 
@@ -76,10 +77,10 @@ begin
     rst <= '1'; wait for 1 ns; rst <= '0'; assert cache_miss_en = '0';
 
     -- read with empty cache
-    a <= X"00000008"; wait for 1 ns; assert rd = all_x; assert cache_miss_en = '1';
-    a <= X"00000008"; we <= '0'; wait for 1 ns; assert rd = all_x; assert cache_miss_en = '1';
+    a <= X"00000008"; wait for 1 ns; assert rd = all_x; assert cache_miss_en = '1'; assert valid_flag = '0';
+    a <= X"00000008"; we <= '0'; wait for 1 ns; assert rd = all_x; assert cache_miss_en = '1'; assert valid_flag = '0';
     -- write with empty cache
-    a <= X"00000008"; wd <= X"0000000F"; we <= '1'; wait for 1 ns; assert cache_miss_en = '1';
+    a <= X"00000008"; wd <= X"0000000F"; we <= '1'; wait for 1 ns; assert cache_miss_en = '1'; assert valid_flag = '0';
 
     a <= X"00000008";
     wd_d1 <= X"00000010"; wd_d2 <= X"00000011"; wd_d3 <= X"00000012"; wd_d4 <= X"00000013";
@@ -97,13 +98,11 @@ begin
     we <= '0'; wait for 1 ns; assert rd = X"FFFFFFFF"; assert cache_miss_en = '0';
 
     -- cache miss
-    a <= X"00001" & X"00C"; we <= '0'; wait for 1 ns; assert rd = all_x; assert cache_miss_en = '1';
+    a <= X"00001" & X"00C"; we <= '0'; wait for 1 ns; assert rd = all_x; assert cache_miss_en = '1'; assert valid_flag = '1';
     -- cache hit
     a <= X"00000008"; we <= '0'; wait for 1 ns; assert rd = X"00000012"; assert cache_miss_en = '0';
     -- cache miss again
-    a <= X"00001" & X"00C"; we <= '1'; wd <= X"FFFFFFFE"; wait for 1 ns; assert cache_miss_en = '1';
-
-
+    a <= X"00001" & X"00C"; we <= '1'; wd <= X"FFFFFFFE"; wait for 1 ns; assert cache_miss_en = '1'; assert valid_flag = '1';
 
     -- skip
     stop <= TRUE;
