1c5ada13bdbd06f142b3801274f3a2809be8a61a
knknkn <knknkn1162@gmail.com>
2018-12-06 16:14:04 +0900

    enhance flopen_controller

--- controller/flopen_controller.vhdl
+++ controller/flopen_controller.vhdl
@@ -3,17 +3,87 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity flopen_controller is
   port (
+    clk, rst, load : in std_logic;
     suspend_flag : in std_logic;
+    stall_flag : in std_logic;
     fetch_en, decode_en, calc_en, dcache_en : out std_logic
   );
 end entity;
 
 architecture behavior of flopen_controller is
-  signal work_en : std_logic;
+  type statetype is (
+    ResetS, LoadS, SuspendS, NormalS, StallS, ErrorS
+  );
+  signal state, nextstate : statetype;
+
 begin
-  work_en <= (not suspend_flag);
-  fetch_en <= work_en;
-  decode_en <= work_en;
-  calc_en <= work_en;
-  dcache_en <= work_en;
+  process(clk, rst, nextstate)
+  begin
+    if rst = '1' then
+      state <= ResetS;
+    elsif rising_edge(clk) then
+      state <= nextstate;
+    end if;
+  end process;
+
+  -- nextstate
+  process(state)
+  begin
+    case state is
+      when ResetS =>
+        if load = '1' then
+          nextstate <= LoadS;
+        else
+          nextstate <= ResetS;
+        end if;
+      when LoadS =>
+        nextstate <= NormalS;
+      when NormalS =>
+        if suspend_flag = '1' then
+          nextstate <= SuspendS;
+        elsif stall_flag = '1' then
+          nextstate <= StallS;
+        else
+          nextstate <= NormalS;
+        end if;
+      when SuspendS =>
+        if suspend_flag = '0' then
+          nextstate <= NormalS;
+        else
+          nextstate  <= SuspendS;
+        end if;
+      when StallS =>
+        nextstate <= NormalS;
+      when others =>
+        nextstate <= ErrorS;
+    end case;
+  end process;
+
+  -- **_en
+  process(state)
+    variable fetch_en0, decode_en0, calc_en0, dcache_en0 : std_logic;
+  begin
+    fetch_en0 := '1';
+    decode_en0 := '1';
+    calc_en0 := '1';
+    dcache_en0 := '1';
+    case state is
+      when SuspendS =>
+        fetch_en0 := '0';
+        decode_en0 := '0';
+        calc_en0 := '0';
+        dcache_en0 := '0';
+      -- wait for MemRead in lw instruction
+      when StallS =>
+        fetch_en0 := '0';
+        decode_en0 := '0';
+        calc_en0 := '0';
+      when others =>
+        -- do nothing
+    end case;
+    fetch_en <= fetch_en0;
+    decode_en <= decode_en0;
+    calc_en <= calc_en0;
+    dcache_en <= dcache_en0;
+  end process;
 end architecture;
