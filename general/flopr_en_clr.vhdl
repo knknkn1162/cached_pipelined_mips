b8db7ddb4181b775a73c49c8e2e5c775304c6466
knknkn <knknkn1162@gmail.com>
2018-12-08 22:50:53 +0900

    Revert "impl flopr_clr without en instead"
    This reverts commit bdc5d54411b3a1da038242fce99f65610e5bd504.

--- general/flopr_en_clr.vhdl
+++ general/flopr_en_clr.vhdl
@@ -1,17 +1,17 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity flopr_clr is
+entity flopr_en_clr is
   generic(N : natural);
   port (
-    clk, rst, clr: in std_logic;
+    clk, rst, en, clr: in std_logic;
     a : in std_logic_vector(N-1 downto 0);
     y : out std_logic_vector(N-1 downto 0)
   );
 end entity;
 
 
-architecture behavior of flopr_clr is
+architecture behavior of flopr_en_clr is
 begin
   process(clk, rst) begin
     if rst='1' then
@@ -19,7 +19,7 @@ begin
     elsif rising_edge(clk) then
       if clr = '1' then
         y <= (others => '0');
-      else
+      elsif en = '1' then
         y <= a;
       end if;
     end if;
